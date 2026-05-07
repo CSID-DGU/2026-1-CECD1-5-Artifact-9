4dd0672b33c4e6ea3175177ce6123e08bbda1ccb
April-37 <150885908+April-37@users.noreply.github.com>
2026-05-07 14:05:42 +0900

    design: 배경색상 및 input 디자인 변경

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from "../types/mainTypes";
 
 export const Card = ({ title, children }: CardProps) => {
   return (
-    <div className="bg-gray-800 rounded-xl p-4 flex flex-col gap-3">
+    <div className="bg-card-bg rounded-xl p-4 flex flex-col gap-3">
       {title && <div className="text-sm font-semibold text-white">{title}</div>}
       {children}
     </div>
