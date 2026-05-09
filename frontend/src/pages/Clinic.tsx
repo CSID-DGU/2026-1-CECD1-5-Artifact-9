674c3efb6de354b47a446958256abb5938007f7e
April-37 <150885908+April-37@users.noreply.github.com>
2026-05-09 20:57:01 +0900

    feat: 진료페이지, 접수페이지 분리

---
+++ frontend/src/pages/Clinic.tsx
@@ -0,0 +1,7 @@
+export default function Clinic() {
+  return (
+    <div className="p-4 flex h-full items-center justify-center">
+      <h2 className="text-2xl font-bold text-gray-500">진료 페이지</h2>
+    </div>
+  );
+}
